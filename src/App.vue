6e903beb9752631b5fc849332158a1870f71a72b
Gabriel Hess <hessg1@bfh.ch>
2019-03-27 11:45:55 +0100

    navigation drawer as menu implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <v-app color="#e5f9e0">
-    <v-toolbar app>
+    <v-navigation-drawer app width=250 mobile-break-point=800 v-model="drawer">
+    <v-toolbar flat>
+      <v-btn icon @click="drawer = !drawer"><v-icon>arrow_back</v-icon></v-btn>
+    </v-toolbar>
+    <v-list  class="pt-0">
+      <v-list-tile
+        v-for="item in menu_items"
+        :key="item.title"
+        :to="item.target"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app clipped-left>
+      <v-btn icon @click="drawer = !drawer"><v-icon>menu</v-icon></v-btn>
       <v-toolbar-title class="headline">
         <span class="font-weight-light">anakoda</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat to="/">home</v-btn>
-      <v-btn flat to="/yourdata">Daten erfassen</v-btn>
     </v-toolbar>
+
 
     <v-content>
       <router-view/>
@@ -28,7 +48,13 @@ export default {
   },
   data () {
     return {
-      //
+      menu_items: [
+          { title: 'Home', icon: 'home', target:"/" },
+          { title: 'Dein Tag erfassen', icon: 'today', target:"/yourdata" },
+          { title: 'Symptome erfassen', icon: 'mood', target:"/yourdata" },
+          { title: 'Impressum', icon: 'info', target:"/about" },
+        ],
+      drawer: "true"
     }
   }
 }
