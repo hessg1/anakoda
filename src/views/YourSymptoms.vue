d8154b9e9305a9ce3b5b9841ebb33b18a506922a
schwf3 <schwf3@bfh.ch>
2019-04-01 17:04:21 +0200

    UI changes

--- src/views/YourSymptoms.vue
+++ src/views/YourSymptoms.vue
@@ -19,8 +19,7 @@
               :items="entryoption"
               menu-props="auto"
               single-line
-              color="#0a967a"
-              >
+              color="#0a967a">
             </v-select>
           </v-flex>
           <v-flex>
@@ -34,8 +33,7 @@
             offset-y
             full-width
             min-width="290px"
-            v-if="entry === 'einen anderen Tag'"
-            >
+            v-if="entry === 'einen anderen Tag'">
             <template v-slot:activator="{ on }">
               <v-text-field
               v-model="dateFormatted"
@@ -46,8 +44,7 @@
               v-on="on"
               @blur="date = parseDate(dateFormatted)"
               @focus="hideKeyboard()"
-              color="#0a967a"
-              ></v-text-field>
+              color="#0a967a"></v-text-field>
             </template>
             <v-date-picker
             v-model="date"
@@ -56,8 +53,7 @@
             @input="menudate = false"
             locale="de-ch"
             first-day-of-week="1"
-            :max="today"
-            ></v-date-picker>
+            :max="today"></v-date-picker>
           </v-menu>
         </v-flex>
       </v-card-text>
@@ -65,157 +61,280 @@
         <v-btn
         color="#0a967a"
         class="white--text"
-        @click="pos = 2"
-        >
-        Weiter
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        @click="pos = 2">Weiter</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-stepper-content>
+
+  <v-stepper-step :complete="pos > 2" step="2" editable color="#0a967a">
+    Kopfschmerzen
+    <small>Hattest du Kopfschmerzen?</small>
+  </v-stepper-step>
+
+  <v-stepper-content step="2">
+    <v-card>
+      <v-card-text>
+        <v-flex>
+          <v-dialog v-model="dialogHeadache" persistent max-width="600px">
+            <template v-slot:activator="{ on }">
+              <v-radio-group v-model="headache" column align-center v-on="on">
+                <v-radio v-for="i in headoption" :key="i.de"  :label="i.de" :value="i" color="#0a967a"></v-radio>
+              </v-radio-group>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Weitere Angaben</span>
+              </v-card-title>
+              <v-card-text>
+                <v-layout row wrap  >
+                  <v-flex xs12 md6>
+                    <v-menu
+                    ref="menutimestarth"
+                    v-model="menutimestarth"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    :return-value.sync="timestart"
+                    lazy
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    max-width="290px"
+                    min-width="290px">
+                    <template v-slot:activator="{ on }">
+                      <v-text-field
+                      v-model="timestart"
+                      label="Startzeit"
+                      prepend-icon="access_time"
+                      readonly
+                      v-on="on"
+                      @focus="hideKeyboard()"
+                      color="#0a967a"/>
+                    </template>
+                    <v-time-picker
+                    v-if="menutimestarth"
+                    v-model="timestart"
+                    format="24hr"
+                    color="#0a967a"
+                    full-width
+                    :max="timemax"
+                    @click:minute="$refs.menutimestarth.save(timestart)"/>
+                  </v-menu>
+                </v-flex>
+                <v-spacer></v-spacer>
+                <v-flex xs12 md6>
+                  <v-menu
+                  ref="menutimeendh"
+                  v-model="menutimeendh"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  :return-value.sync="timeend"
+                  lazy
+                  transition="scale-transition"
+                  offset-y
+                  full-width
+                  max-width="290px"
+                  min-width="290px">
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                    v-model="timeend"
+                    label="Endzeit"
+                    prepend-icon="access_time"
+                    readonly
+                    @focus="hideKeyboard()"
+                    v-on="on"
+                    color="#0a967a"/>
+                  </template>
+                  <v-time-picker
+                  v-if="menutimeendh"
+                  v-model="timeend"
+                  format="24hr"
+                  color="#0a967a"
+                  full-width
+                  :min= "timemin"
+                  @click:minute="$refs.menutimeendh.save(timeend)"/>
+                </v-menu>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+          <v-card-text>
+            <v-subheader>Kopfseite</v-subheader>
+            <v-select
+            v-model="hsite"
+            :items="hsiteoption"
+            item-text="de"
+            menu-props="auto"
+            single-line
+            color="#0a967a"
+            return-object>
+          </v-select>
+        </v-card-text>
+        <v-card-text>
+          <v-subheader>Intensität</v-subheader>
+          <v-slider
+          v-model="quality"
+          thumb-label="always"
+          min="0"
+          max="10"
+          append-icon="mood_bad"
+          prepend-icon="mood"
+          color="#0a967a"
+          @click:append="qualityPlus"
+          @click:prepend="qualityMinus"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary darken-1" flat @click="closeDialog()">Abbrechen</v-btn>
+          <v-btn color="primary darken-1" flat @click="saveCurrentHeadache()">Fertig</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-radio-group v-model="headache" column align-center>
+      <v-radio label="Keine Kopfschmerzen" :value="noheadache" color="#0a967a"></v-radio>
+    </v-radio-group>
+  </v-flex>
+</v-card-text>
+<v-card-actions>
+  <v-btn
+  color="#0a967a"
+  class="white--text"
+  @click="pos = 3">Weiter</v-btn>
+</v-card-actions>
+</v-card>
 </v-stepper-content>
 
-<v-stepper-step :complete="pos > 2" step="2" editable color="#0a967a">
-  Symptome
-  <small>Welche Symptome sind dir aufgefallen?</small>
+<v-stepper-step :complete="pos > 3" step="3" editable color="#0a967a">
+  Andere Symptome
+  <small>Welche anderen Symptome sind dir aufgefallen?</small>
 </v-stepper-step>
 
-<v-stepper-content step="2">
+<v-stepper-content step="3">
   <v-card>
     <v-card-text>
       <v-layout wrap>
-      <v-dialog v-model="dialog" persistent max-width="600px">
-        <template v-slot:activator="{ on }">
-          <v-select
-          v-model="symptoms"
-          :items="symoption"
-          item-text="de"
-          label="Deine Symptome"
-          chips
-          clearable
-          solo
-          multiple
-          return-object
-          >
-          <template v-slot:selection="data">
-            <v-badge color="red" overlap>
-              <template v-slot:badge v-if="data.item.flag == false">
-                <v-icon
-                dark
-                small
-                >
-                warning
-              </v-icon>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+            <v-select
+            v-model="symptoms"
+            :items="symoption"
+            item-text="de"
+            label="Deine Symptome"
+            chips
+            clearable
+            solo
+            multiple
+            return-object>
+            <template v-slot:selection="data">
+              <v-badge color="red" overlap>
+                <template v-slot:badge v-if="data.item.flag == false">
+                  <v-icon
+                  dark
+                  small>
+                  warning
+                </v-icon>
+              </template>
+              <v-chip
+              :selected="data.selected.de"
+              close
+              @input="remove(data.item)"
+              @click="setCurrent(data.item)"
+              v-on="on"
+              outline
+              color="#0a967a"
+              class="text-truncate">
+              <strong>{{ data.item.de }}</strong>
+              <span v-if="data.item.flag == true && data.item.category != 'Condition'" class="hidden-sm-and-down text-truncate">&nbsp;{{data.item.quality}}</span>
+              <span v-if="data.item.flag == true" class="hidden-sm-and-down text-truncate">&nbsp;({{data.item.start}} - {{data.item.end}})</span>
+            </v-chip>
+          </v-badge>
+        </template>
+      </v-select>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Weitere Angaben</span>
+      </v-card-title>
+      <v-card-text>
+        <v-layout row wrap  >
+          <v-flex xs12 md6>
+            <v-menu
+            ref="menutimestart"
+            v-model="menutimestart"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            :return-value.sync="timestart"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            max-width="290px"
+            min-width="290px">
+            <template v-slot:activator="{ on }">
+              <v-text-field
+              v-model="timestart"
+              label="Startzeit"
+              prepend-icon="access_time"
+              readonly
+              v-on="on"
+              @focus="hideKeyboard()"
+              color="#0a967a"/>
             </template>
-            <v-chip
-            :selected="data.selected.de"
-            close
-            @input="remove(data.item)"
-            @click="setCurrent(data.item)"
-            v-on="on"
-            outline
+            <v-time-picker
+            v-if="menutimestart"
+            v-model="timestart"
+            format="24hr"
             color="#0a967a"
-            class="text-truncate"
-            >
-            <strong>{{ data.item.de }}</strong>
-            <span v-if="data.item.flag == true && data.item.category != 'Condition'" class="hidden-sm-and-down text-truncate">&nbsp;{{data.item.quality}}</span>
-            <span v-if="data.item.flag == true" class="hidden-sm-and-down text-truncate">&nbsp;({{data.item.start}} - {{data.item.end}})</span>
-          </v-chip>
-        </v-badge>
-      </template>
-    </v-select>
-  </template>
-  <v-card>
-    <v-card-title>
-      <span class="headline">Weitere Angaben</span>
-    </v-card-title>
-    <v-card-text>
-      <v-layout row wrap  >
+            full-width
+            :max="timemax"
+            @click:minute="$refs.menutimestart.save(timestart)"/>
+          </v-menu>
+        </v-flex>
+        <v-spacer></v-spacer>
         <v-flex xs12 md6>
           <v-menu
-          ref="menutimestart"
-          v-model="menutimestart"
+          ref="menutimeend"
+          v-model="menutimeend"
           :close-on-content-click="false"
           :nudge-right="40"
-          :return-value.sync="timestart"
+          :return-value.sync="timeend"
           lazy
           transition="scale-transition"
           offset-y
           full-width
           max-width="290px"
-          min-width="290px"
-          >
+          min-width="290px">
           <template v-slot:activator="{ on }">
             <v-text-field
-            v-model="timestart"
-            label="Startzeit"
+            v-model="timeend"
+            label="Endzeit"
             prepend-icon="access_time"
             readonly
-            v-on="on"
             @focus="hideKeyboard()"
-            color="#0a967a"
-            />
+            v-on="on"
+            color="#0a967a"/>
           </template>
           <v-time-picker
-          v-if="menutimestart"
-          v-model="timestart"
+          v-if="menutimeend"
+          v-model="timeend"
           format="24hr"
           color="#0a967a"
           full-width
-          :max="timemax"
-          @click:minute="$refs.menutimestart.save(timestart)"
-          />
+          :min= "timemin"
+          @click:minute="$refs.menutimeend.save(timeend)"/>
         </v-menu>
       </v-flex>
-      <v-spacer></v-spacer>
-      <v-flex xs12 md6>
-        <v-menu
-        ref="menutimeend"
-        v-model="menutimeend"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        :return-value.sync="timeend"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        max-width="290px"
-        min-width="290px"
-        >
-        <template v-slot:activator="{ on }">
-          <v-text-field
-          v-model="timeend"
-          label="Endzeit"
-          prepend-icon="access_time"
-          readonly
-          @focus="hideKeyboard()"
-          v-on="on"
-          color="#0a967a"
-          />
-        </template>
-        <v-time-picker
-        v-if="menutimeend"
-        v-model="timeend"
-        format="24hr"
-        color="#0a967a"
-        full-width
-        :min= "timemin"
-        @click:minute="$refs.menutimeend.save(timeend)"
-        />
-      </v-menu>
-    </v-flex>
-  </v-layout>
-</v-card-text>
-<v-card-text v-if= "this.symcurrentid != null && this.symptoms[this.symcurrentid].category == 'Headache'">
-  <v-subheader>Kopfseite</v-subheader>
-  <v-select
-  v-model="hsite"
-  :items="hsiteoption"
-  item-text="de"
-  menu-props="auto"
-  single-line
-  color="#0a967a"
-  return-object
-  >
-</v-select>
+    </v-layout>
+  </v-card-text>
+  <v-card-text v-if= "this.symcurrentid != null && this.symptoms[this.symcurrentid].category == 'Headache'">
+    <v-subheader>Kopfseite</v-subheader>
+    <v-select
+    v-model="hsite"
+    :items="hsiteoption"
+    item-text="de"
+    menu-props="auto"
+    single-line
+    color="#0a967a"
+    return-object>
+  </v-select>
 </v-card-text>
 <v-card-text v-if="this.symcurrentid != null && this.symptoms[this.symcurrentid].category != 'Condition'">
   <v-subheader>Intensität</v-subheader>
@@ -228,8 +347,7 @@
   prepend-icon="mood"
   color="#0a967a"
   @click:append="qualityPlus"
-  @click:prepend="qualityMinus"
-  />
+  @click:prepend="qualityMinus"/>
 </v-card-text>
 <v-card-actions>
   <v-spacer></v-spacer>
@@ -254,8 +372,7 @@
 v-if="midata != null && controll == true && midata.isReady()"
 color="#0a967a"
 class="white--text"
-@click="save()"
->
+@click="save()">
 Speichern
 <v-icon right dark>save</v-icon>
 </v-btn>
@@ -298,9 +415,11 @@ export default {
     timestart: null,
     timestartFormatted: null,
     menutimestart: false,
+    menutimestarth: false,
     timeend: null,
     timeendFormatted: null,
     menutimeend: false,
+    menutimeendh: false,
     quality: 0,
     snomedservice: null,
     symptoms: [],
@@ -314,7 +433,11 @@ export default {
     midata: null,
     snackbartext: "",
     snackbar: false,
-    snackbarcolor: ''
+    snackbarcolor: '',
+    dialogHeadache: false,
+    headache: {de: 'Keine Kopfschmerzen', flag:true},
+    noheadache: {de: 'Keine Kopfschmerzen', flag:true},
+    headoption: []
   }),
 
   watch: {
@@ -384,9 +507,9 @@ export default {
     /*
     Corrects the day of a date (as iso-string) by a given value, taking into account
     parameters: - date: the original date, as a string compatible to the Date()
-                        constructor (e.g ISO8601: YYYY-MM-DD)
-                - val: the number of days to correct the date by (e.g. -1 for
-                       yesterday)
+    constructor (e.g ISO8601: YYYY-MM-DD)
+    - val: the number of days to correct the date by (e.g. -1 for
+    yesterday)
     returns:    - a date as a string in ISO8601 (YYYY-MM-DD)
     author:     schwf3, refactoring hessg1
     version:    2019-03-26
@@ -521,6 +644,25 @@ export default {
     },
 
     /*
+    Save values of headache.
+    Controll the data and set values to default and close dialog.
+    author:     schwf3
+    version:    2019-03-27
+    */
+    saveCurrentHeadache() {
+      this.headache.start = this.timestart;
+      this.headache.end = this.timeend;
+      this.headache.startFormatted = this.timestartFormatted;
+      this.headache.endFormatted = this.timeendFormatted;
+      this.headache.quality = this.quality;
+      this.headache.bodysite = this.hsite;
+      this.quality = 0;
+      this.hsite = null;
+      this.dialogHeadache = false;
+    },
+
+
+    /*
     Set values to default and close dialog.
     author:     schwf3
     version:    2019-03-27
@@ -528,7 +670,10 @@ export default {
     closeDialog(){
       this.quality = 0;
       this.hsite = null;
+      this.timestart = null;
+      this.timeend = null;
       this.dialog = false;
+      this.dialogHeadache = false;
     },
 
     /*
@@ -552,6 +697,10 @@ export default {
             observations[i] = new Headache(this.symptoms[i].startFormatted, this.symptoms[i].endFormatted, this.symptoms[i].quality, this.symptoms[i].code, this.symptoms[i].bodysite.code);
           }
         }
+        if(this.headoption.includes(this.headache)){
+          var ha = new Headache(this.headache.startFormatted, this.headache.endFormatted, this.headache.quality, this.headache.code, this.headache.bodysite.code);
+          observations.push(ha);
+        }
         let bundle = this.midata.bundle(observations);
         this.midata.saveData(bundle)
         .then(res => {
@@ -568,7 +717,7 @@ export default {
     Displays a SnackBar to the user. If the error parameter is set to true, the
     message is displayed in red.
     parameters: - text: the text displayed to the user
-                - error: boolean, if it is an error message (message in red)
+    - error: boolean, if it is an error message (message in red)
     returns:    nothing
     author:     hessg1
     version:    2019-03-27
@@ -589,18 +738,25 @@ export default {
     hideKeyboard(){
       document.activeElement.blur();
     },
+    setCurrentHead(){
+
+    }
   },
 
   // mounted is executed when component is mounted
   mounted(){
     this.snomedservice = new SnomedService();
-    this.symoption = this.snomedservice.getFiltered(x => x.category == "Headache" || x.category == "VariousComplaint" || x.category == "Condition");
+    this.symoption = this.snomedservice.getFiltered(x => x.category == "VariousComplaint" || x.category == "Condition");
+    this.headoption = this.snomedservice.getFiltered(y => y.category == "Headache");
     for(var i in this.symoption){
       this.symoption[i].flag = false;
     }
+    for(var j in this.headoption){
+      this.headoption[j].flag = false;
+    }
     this.hsiteoption = this.snomedservice.getFiltered(x => x.category == "BodySite");
-    for(i in this.hsiteoption){
-      this.hsiteoption[i].flag = false;
+    for(var y in this.hsiteoption){
+      this.hsiteoption[y].flag = false;
     }
     this.midata = new MidataService();
   }
