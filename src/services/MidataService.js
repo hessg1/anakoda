94b671402ebe14635dfcb305e75f5f1796d77fd6
Gabriel Hess <hessg1@bfh.ch>
2019-04-10 10:13:26 +0200

    201 / 202: retrieve and convert data Observation and Patient Resources are now transferred to a easier usable data model

--- src/services/MidataService.js
+++ src/services/MidataService.js
@@ -1,6 +1,9 @@
-import $ from 'jquery';
 const serviceUri = "https://test.midata.coop/fhir/";
 const client = "anakoda";
+
+import $ from 'jquery';
+import SnomedService from '@/services/SnomedService';
+const sct = new SnomedService();
 
 export default class MidataService {
   /*
@@ -182,40 +185,42 @@ export default class MidataService {
   /*
     Retrieves Data from MIDATA.
     Requires a completely set up MIDATA environment, with Token etc.
-    TODO: detailed description
     parameters  - query: a string with the wanted midata-query
-    returns     TODO
+    returns     - a promise with either the FHIR bundle as returned from
+                  MIDATA (resolve) or an error (reject)
     author      hessg1
-    version     2019-03-13
+    version     2019-04-09
   */
   getData(query){
     if(!query || query == ''){
       throw("Kann keine leere Anfrage machen.");
     }
-    const url = this.uri.service + "/" + query;
-    const header =  "Bearer " + this.token;
-
     if(this.token == ""){
-      console.log("please... get a token.");
+      throw("please... get a token.");
       // TODO: handle missing token (e.g. get authorization)
     }
     else {
-      // ajax-request to midata
-      $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "json",
-        headers: {
-            "Authorization": header
-        },
-      }).done(res => {
-        console.log("done");
-        console.log(res);
-      })
-      .catch(err => {
-        console.log("Error: " + err.responseText);
-        throw(err);
+      const url = this.uri.service + "/" + query;
+      const header =  "Bearer " + this.token;
+
+      return new Promise(function(resolve, reject){
+        // ajax-request to midata
+        $.ajax({
+          url: url,
+          type: "GET",
+          dataType: "json",
+          headers: {
+              "Authorization": header
+          },
+        }).done(res => {
+          resolve(res);
+        })
+        .catch(err => {
+          console.log("Error: " + err.responseText);
+          reject(err);
+        });
       });
+
     }
   }
 
@@ -314,6 +319,94 @@ export default class MidataService {
     }
     else{
       throw("Invalid argument: \"resources\" must be an Array with at least one element");
+    }
+  }
+
+  /*
+  Processes a FHIR bundle into easier to process JSON objects with all relevant data
+  parameters  - res: a FHIR bundle as retrieved from MIDATA (and as returned
+                     from getData()), containing Patient and / or Observation resources.
+  returns     - an array of Objects, containing Observations and / or patient object
+                (depending on resources in bundle)
+  throws      - an error if the bundly contains any non patient- or observation resource
+  author      hessg1
+  version     2019-04-10
+  */
+  prepareData(res){
+    let data = [];
+    for(var i in res.entry){
+      if(res.entry[i].resource.resourceType == 'Observation'){
+        // create template object from SnomedService
+        let code = "";
+        if(res.entry[i].resource.valueCodeableConcept != undefined){
+          code = res.entry[i].resource.valueCodeableConcept.coding[0].code;
+        }
+        else {
+          code = res.entry[i].resource.component[0].code.coding[0].code;
+        }
+
+        // get the template for the sct code from snomed service
+        let template = sct.getFiltered(x => (x.code == code));
+
+        // if code was not found in SNOMED CT, we abort and throw an error:
+        if(template.length == 0){
+          throw("Fehlerhafte Daten: SNOMED-Code " + code + " ist nicht bekannt (Objekt " + i + ").");
+        }
+        template = template[0]; // we only expect this array to have one entry anyway, since code is unique
+
+        // fill template with actual values from resource
+        if(template.category == 'EatingHabit'){ // EatingHabit has only one Time
+          template.date = new Date(res.entry[i].resource.effectiveDateTime);
+        }
+        else if(template.category == 'Diagnosis'){ // so does Diagnosis, but encoded differently
+          template.date = new Date(res.entry[i].resource.component.valueDateTime);
+        }
+        else { // all other have start and end times
+          template.startTime = new Date(res.entry[i].resource.effectivePeriod.start);
+          template.endTime = new Date(res.entry[i].resource.effectivePeriod.end);
+        }
+
+        if(template.category == 'VariousComplaint' || template.category == 'Headache' || template.category == 'SleepPattern'){
+          // these Categories have intensities
+          template.quantity = res.entry[i].resource.component[0].valueQuantity.value;
+        }
+
+        // create metadata
+        let meta = {};
+        meta.id = res.entry[i].resource.id;
+        meta.versionId = res.entry[i].resource.meta.versionId;
+        meta.timestamp = res.entry[i].resource.meta.lastUpdated;
+        meta.source = res.entry[i].resource.meta.extension[0].extension[0].valueCoding.display;
+        template.meta = meta;
+        data.push(template);
+      }
+      else if(res.entry[i].resource.resourceType == 'Patient'){
+        let pat = {};
+        pat.id = res.entry[i].resource.id;
+        pat.name = res.entry[i].resource.name[0].family;
+        pat.firstName = res.entry[i].resource.name[0].given[0];
+        pat.fullName = pat.firstName + " " + pat.name;
+        pat.birthDate = res.entry[i].resource.birthDate
+
+        let meta = {};
+        meta.versionId = res.entry[i].resource.meta.versionId;
+        meta.timestamp = res.entry[i].resource.meta.lastUpdated;
+        meta.participantId = "";
+        // identifier is an array of various identifiers in MIDATA
+        let ident = res.entry[i].resource.identifier;
+        for(var entry in ident){
+          // we want the participant-name the user has in a study
+          if (ident[entry].system == 'http://midata.coop/identifier/participant-name'){
+            meta.participantId = ident[entry].value;
+          }
+        }
+        pat.meta = meta;
+        data.push(pat);
+      }
+      else{
+        throw("Fehler: Kann momentan nur Bundles mit Observation- oder Patient-Ressourcen verarbeiten.");
+      }
+      return data;
     }
   }
 
