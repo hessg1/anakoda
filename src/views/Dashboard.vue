7c79f7ca8c033ab12a0fe63cd92891ad80b19745
schwf3 <schwf3@bfh.ch>
2019-04-29 14:44:37 +0200

    402 Dashboard View

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,44 +1,22 @@
-<template><div>
-<v-img :src="require('../assets/dashboard_prvw.png')"></v-img>
-<v-dialog
-      v-model="dialog"
-      width="500"
-      persistent
-    >
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          coming soon...
-        </v-card-title>
+<template>
+  <div>
+    <v-tabs color="#40c9a2" slider-color="#a3f7b5">
+    <v-tab>Gesamtgrafik</v-tab>
 
-        <v-card-text>
-          <p>Das Dashboard ist in anakoda 0.2 noch nicht verfügbar.</p>
-          <p>
-          Das Dashboard wird in einer ersten
-          Version mit anakoda 0.3 eingeführt, die im Mai erscheint. Bis dann kannst du
-          deine Daten unter <a href="#/dataview">Meine Daten</a> einsehen.
+    <!-- overall graphic -->
+    <v-tab-item>
 
-        </p>
-        </v-card-text>
+    </v-tab-item>
 
-        <v-divider></v-divider>
+    <v-tab>Kennzahlen</v-tab>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            flat
-            :to="'/'"
-          >
-            OK
-          </v-btn>
-        </v-card-actions>
+    <!-- overall graphic -->
+    <v-tab-item>
 
-      </v-card>
-    </v-dialog>
-</div>
+    </v-tab-item>
+  </v-tabs>
+
+  </div>
 </template>
 
 <script>
