80dbfcb6f78354d1587edae79a5cc441d250d851
Gabriel Hess <hessg1@bfh.ch>
2019-04-30 18:04:23 +0200

    added colours to SnomedService

--- src/services/SnomedService.js
+++ src/services/SnomedService.js
@@ -18,364 +18,416 @@ export default class SnomedService {
         "en": "Sees flickering lights",
         "de": "Flimmersehen",
         "superCategory": 281004000,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 74964007,
         "en": "Other symptom",
         "de": "Andere Symptome",
         "superCategory": 118222006,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 162057007,
         "en": "Nausea present",
         "de": "Übelkeit",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 248254009,
         "en": "Sleep pattern finding",
         "de": "Schlafmuster",
         "superCategory": "65554-8",
-        "category": "SleepPattern"
+        "category": "SleepPattern",
+        "colour": "#921B51"
       },
       {
         "code": "65554-8",
         "en": "Sleep / wakeup duration",
         "de": "Schlafdauer",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 409668002,
         "en": "Photophobia",
         "de": "Lichtempfindlichkeit",
         "superCategory": 246613004,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 313387002,
         "en": "Phonophobia",
         "de": "Lärmempfindlichkeit",
         "superCategory": 285854004,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 267092007,
         "en": "Has eye discharge",
         "de": "Tränende Augen",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 267093002,
         "en": "Has a red eye",
         "de": "Gerötete Augen",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 276319003,
         "en": "Menstruation finding",
         "de": "Menstruation",
         "superCategory": 66523006,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 216299002,
         "en": "Attack",
         "de": "Migräneanfall",
         "superCategory": 118222006,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#FD978F"
       },
       {
         "code": 73595000,
         "en": "Stress",
         "de": "Stress",
         "superCategory": 363870007,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 102894008,
         "en": "Feeling calm",
         "de": "Entspannt",
         "superCategory": 285854004,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 279079003,
         "en": "Dysesthesia",
         "de": "Gefühlsstörung",
         "superCategory": 247325003,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 29164008,
         "en": "Speech Impairment",
         "de": "Sprachstörung",
         "superCategory": 363918005,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 267101005,
         "en": "Nasal discharge present",
         "de": "Nasenlaufen",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 267100006,
         "en": "Nasal obstruction present",
         "de": "Verstopfte Nase",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 119711004,
         "en": "Nose closure",
         "de": "Verstopfte Nase",
         "superCategory": 373573001,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 45846002,
         "en": "Hyperosmia",
         "de": "Geruchsempfindlichkeit",
         "superCategory": 397686008,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 422400008,
         "en": "Vomiting",
         "de": "Erbrechen",
         "superCategory": 405166007,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 16932000,
         "en": "Nausea and vomiting",
         "de": "Übelkeit und Erbrechen",
         "superCategory": 405166007,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 248626009,
         "en": "Yawning",
         "de": "Vermehrtes Gähnen",
         "superCategory": 37993006,
-        "category": "VariousComplaint"
+        "category": "VariousComplaint",
+        "colour": "#FED37F"
       },
       {
         "code": 309253009,
         "en": "Difficulty reading",
         "de": "Leseschwierigkeiten",
         "superCategory": 363870007,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 106126000,
         "en": "Emotional state finding",
         "de": "Emotionale Empfindlichkeit",
         "superCategory": 285854004,
-        "category": "Condition"
+        "category": "Condition",
+        "colour": "#D5FA80"
       },
       {
         "code": 162308004,
         "en": "Throbbing Headache",
         "de": "Pulsierende Kopfschmerzen",
         "superCategory": "",
-        "category": "Headache"
+        "category": "Headache",
+        "colour": "#7B84FC"
       },
       {
         "code": 162307009,
         "en": "Aching Headache",
         "de": "Stechende Kopfschmerzen",
         "superCategory": "",
-        "category": "Headache"
+        "category": "Headache",
+        "colour": "#79FDFE"
       },
       {
         "code": 162309007,
         "en": "Shooting Headache",
         "de": "Einschiessende Kopfschmerzen",
         "superCategory": "",
-        "category": "Headache"
+        "category": "Headache",
+        "colour": "#7AD6FD"
       },
       {
         "code": 702970004,
         "en": "Eating habit unknown",
         "de": "Unbekanntes Essverhalten",
         "superCategory": "",
-        "category": "EatingHabit"
+        "category": "EatingHabit",
+        "colour": "##928F1B"
       },
       {
         "code": 289141003,
         "en": "Eats regularly",
         "de": "Regelmässig gegessen",
         "superCategory": "",
-        "category": "EatingHabit"
+        "category": "EatingHabit",
+        "colour": "##928F1B"
       },
       {
         "code": 225526009,
         "en": "Eats irregularly",
         "de": "Unregelmässig gegessen",
         "superCategory": "",
-        "category": "EatingHabit"
+        "category": "EatingHabit",
+        "colour": "##928F1B"
       },
       {
         "code": 29624005,
         "en": "Right side of head",
         "de": "Rechte Kopfhälfte",
         "superCategory": "",
-        "category": "BodySite"
+        "category": "BodySite",
+        "colour": "#FFFFFF"
       },
       {
         "code": 64237003,
         "en": "Left side of head",
         "de": "Linke Kopfhälfte",
         "superCategory": "",
-        "category": "BodySite"
+        "category": "BodySite",
+        "colour": "#FFFFFF"
       },
       {
         "code": 162301005,
         "en": "Bilateral headache",
         "de": "Beidseitige Kopfschmerzen",
         "superCategory": "",
-        "category": "BodySite"
+        "category": "BodySite",
+        "colour": "#FFFFFF"
       },
       {
         "code": 405166007,
         "en": "Nausea and vomiting status",
         "de": "Übelkeits- und Erbrechensstatus",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 285854004,
         "en": "Emotion",
         "de": "Emotion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 281004000,
         "en": "Visual function",
         "de": "Visuelle Funktion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 363918005,
         "en": "Speech observable",
         "de": "Sprachbeobachtung",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 397686008,
         "en": "Sense of smell, function",
         "de": "Geruchssinnfunktion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 37993006,
         "en": "Yawning, function",
         "de": "Gähnfunktion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 246613004,
         "en": "General reaction to light",
         "de": "Generelle Reaktion auf Licht",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 247325003,
         "en": "Altered sensation of skin",
         "de": "Verändertes Hautempfinden",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 118222006,
         "en": "General finding of observation of patient",
         "de": "Generelle Beobachtung des Patienten",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 373573001,
         "en": "Clinical finding present",
         "de": "Klinischer Befund vorhanden",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 363870007,
         "en": "Mental state, behavior / psychosocial function observable",
         "de": "Beobachtung psychischer Zustand, psychosoziale Funktion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 66523006,
         "en": "Female reproductive function",
         "de": "Funktion weiblicher Reproduktion",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 128187005,
         "en": "Vascular headache",
         "de": "Vaskuläre Kopfschmerzen",
         "superCategory": "",
-        "category": "SuperCategory"
+        "category": "SuperCategory",
+        "colour": "#FFFFFF"
       },
       {
         "code": 37796009,
         "en": "Migraine",
         "de": "Migräne",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "G43.0",
         "en": "Migraine without aura",
         "de": "Migräne ohne Aura",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "G43.1",
         "en": "Migraine with aura",
         "de": "Migräne mit Aura",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "G43.10",
         "en": "Typical aura with headache",
         "de": "Typische Aura mit Kopfschmerzen",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "G43.104",
         "en": "Typical aura without headache",
         "de": "Typische Aura ohne Kopfschmerzen",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "G43.105",
         "en": "Hemiplegic migraine",
         "de": "Hemiplegische Migräne",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       // we have to deal with fake snomed codes for "other diagnosis" and "other
       // headache", because they are all encoded in heMigrania with 74964007 and
@@ -385,14 +437,16 @@ export default class SnomedService {
         "en": "Other diagnosis",
         "de": "Andere Diagnose",
         "superCategory": 128187005,
-        "category": "Diagnosis"
+        "category": "Diagnosis",
+        "colour": "#664147"
       },
       {
         "code": "74964007h",
         "en": "Other Headache",
         "de": "Sonstige Kopfschmerzen",
         "superCategory": "",
-        "category": "Headache"
+        "category": "Headache",
+        "colour": "#79FBD6"
       },
     ];
   }
