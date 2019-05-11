379bffa2911c103ceeef1f6a3677ca1711535941
Gabriel Hess <hessg1@bfh.ch>
2019-05-11 20:49:42 +0200

    Version history updated for V0.3

--- src/components/Versions.vue
+++ src/components/Versions.vue
@@ -1,6 +1,23 @@
 <template>
   <v-container>
     <h1>Versionsverlauf</h1>
+
+    <h2>anakoda 0.3</h2>
+    <p>13.05.2019</p>
+    <p>
+      <ul>
+        <li>Dashboard
+          <ul>
+            <li>Auf einem Zeitstrahl werden all deine erfassten Daten angezeigt</li>
+            <li>Verschiedene Kennzahlen zu deinen Daten werden aufbereitet dargestellt</li>
+          </ul>
+        </li>
+        <li>Einstellungen: Neue Option "bei MIDATA eingeloggt bleiben"</li>
+        <li>Logout-Button im Seitenmenu</li>
+        <li>verschiedene Optimierungen im MidataService</li>
+      </ul>
+    </p>
+
     <h2>anakoda 0.2</h2>
     <p>29.04.2019</p>
     <p>
@@ -13,6 +30,7 @@
         <li>diverse kleinere Anpassungen</li>
         <li>Ein Bug, der das Verbinden mit MIDATA verhinderte, wurde behoben</li>
       </ul>
+    </p>
 
     <h2>anakoda 0.1.1</h2>
     <p>24.04.2019</p>
